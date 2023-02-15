0827af69282c8bf428569d52a93ba83b911935ae
olha-smieshko <smeshkoom@gmail.com>
2023-02-15 11:03:16 +0200

    1

---
+++ js/3.js
@@ -0,0 +1,18 @@
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
+
+const product = {
+  // Change code below this line
+  name,
+  price,
+  image,
+  tags,
+  // Change code above this line
+};
+console.log (product);
+console.log (product.name);
+console.log (product.price);
+console.log (product.image);
+console.log (product.tags);
